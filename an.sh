2b027a9e69de02b8f07037dac21fe2de27123967
Nicholas Jensen-Hay <NicholasJensenHay@gmail.com>
2016-04-05 13:26:26 -0700

    an.sh now creates components, no path

--- an.sh
+++ an.sh
@@ -2,16 +2,22 @@
 
 
 if [ $1 == 'g' ]; then
-
-  FILENAME = $3'.'$2'.ts'
-  echo "import { Component } from 'angular2/core';" >> $FILENAME
-  echo "@Component({" >> $FILENAME
-  echo "  selector: 'my-app'," >> $FILENAME
-  echo "  template: `" >> $FILENAME
-  echo "    <h1>Skeleton Angular2 App!</h1>" >> $FILENAME
-  echo "  `" >> $FILENAME
-  echo "})" >> $FILENAME
-  echo "export class "$3$2" {" >> $FILENAME
-  echo "}" >> $FILENAME
-
+  if[ $2 == 'component' ]; then
+    echo "What is your selector?"
+    read selector
+    FILENAME=$3'.'$2'.ts'
+    CAP3="$(tr '[:lower:]' '[:upper:]' <<< ${3:0:1})${3:1}"
+    CAP2="$(tr '[:lower:]' '[:upper:]' <<< ${2:0:1})${2:1}"
+    NAME="$CAP3$CAP2"
+    echo "import { Component } from 'angular2/core';" >> $FILENAME
+    echo "" >> $FILENAME
+    echo "@Component({" >> $FILENAME
+    echo "  selector: '$selector'," >> $FILENAME
+    echo "  template: `" >> $FILENAME
+    echo "    <div>Template code goes here!</div>" >> $FILENAME
+    echo "  `" >> $FILENAME
+    echo "})" >> $FILENAME
+    echo "export class "$NAME" {" >> $FILENAME
+    echo "}" >> $FILENAME
+  fi
 fi
